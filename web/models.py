82f0ed9de90c6fd70decf4cac2126c778a7e2aea
Neutrolex <60861853campos@gmail.com>
2026-03-20 12:14:04 -0500

    add: agregando archivos iniciales

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    icono = models.CharField(max_length=50, help_text="Nombre de clase FontAwesome o similar")
+    orden = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.titulo
--- web/views.py
+++ web/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Servicio
 
 def home(request):
-    return render(request, 'web/base.html')
+    # Traemos solo los 3 servicios principales para el inicio
+    servicios = Servicio.objects.all()[:3]
+    return render(request, 'web/index.html', {'servicios': servicios})
+
+def nosotros(request):
+    return render(request, 'web/nosotros.html')
+
+def servicios(request):
+    todos_servicios = Servicio.objects.all().order_by('orden')
+    return render(request, 'web/servicios.html', {'servicios': todos_servicios})
+
+def contacto(request):
+    return render(request, 'web/contacto.html')
